229c485f3ea390f72ff66c4aecc6e2620b6da597
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2024-02-25 00:46:42 +0300

    readme

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,7 +4,7 @@ import { BuildOptions } from './types/types'
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
-    open: ['/Calendar_nutri'],
+    open: ['/Calendar_nutri/'],
     historyApiFallback: true,
     hot: true,
     client: {
--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -20,7 +20,7 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
       filename: '[name].[contenthash].js',
       path: paths.output,
       clean: true,
-      publicPath: '/Calendar_nutri',
+      publicPath: '/Calendar_nutri/',
     },
     plugins: buildPlugins(options).filter(Boolean),
     module: {
